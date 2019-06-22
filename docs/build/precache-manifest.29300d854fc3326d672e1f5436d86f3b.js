db8176d52227dc8b3f7d634461329be7da11e3f8
jaresan <jares.antonin@gmail.com>
2019-06-22 13:26:52 +0200

    Set up production build & connected it to GH pages.

---
+++ docs/build/precache-manifest.29300d854fc3326d672e1f5436d86f3b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f6752bff3d3ddca70fdbaf067d52b51",
+    "url": "./index.html"
+  },
+  {
+    "revision": "2f9aca97cb822d179f4f",
+    "url": "./static/css/2.ea2c0e55.chunk.css"
+  },
+  {
+    "revision": "204535622cc88dd8ae1a",
+    "url": "./static/css/main.b7b59061.chunk.css"
+  },
+  {
+    "revision": "2f9aca97cb822d179f4f",
+    "url": "./static/js/2.64dbb63d.chunk.js"
+  },
+  {
+    "revision": "204535622cc88dd8ae1a",
+    "url": "./static/js/main.63da4048.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
