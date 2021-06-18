dd6532aeda1e6e0c9d40f4ad334fbf7865cb5065
Antonin Jares <jares.antonin@gmail.com>
2021-06-18 18:31:49 +0200

    Fix document title Fix query not being shareable until after execution

--- src/@@actions/interactions/yasgui.js
+++ src/@@actions/interactions/yasgui.js
@@ -2,20 +2,21 @@ import { view } from 'ramda';
 import * as YasguiState from '@@app-state/yasgui/state';
 import { getState } from '@@app-state';
 import * as ModelState from '@@app-state/model/state';
+import { indentByBrackets } from '@@string';
 
-export const getDirectFetchUrl = () => {
-  const yasguiInstance = view(YasguiState.instance, getState());
-  return yasguiInstance.getTab().yasr.config.getPlainQueryLinkToEndpoint();
-};
+const tab = () => {
+  const tab = view(YasguiState.instance, getState()).getTab();
+  tab.setQuery(indentByBrackets(view(YasguiState.query, getState())));
 
-export const getCurlFetchString = () => {
-  const yasguiInstance = view(YasguiState.instance, getState());
-  return yasguiInstance.getTab().yasqe.getAsCurlString();
-};
+  return tab;
+}
+
+export const getDirectFetchUrl = () => tab().yasr.config.getPlainQueryLinkToEndpoint();
+
+export const getCurlFetchString = () => tab().yasqe.getAsCurlString();
 
 export const getYasguiShareUrl = () => {
   const endpoint = encodeURIComponent(view(ModelState.endpoint, getState()));
-  const yasguiInstance = view(YasguiState.instance, getState());
-  const query = encodeURIComponent(yasguiInstance.getTab().getQuery());
+  const query = encodeURIComponent(tab().getQuery());
   return `https://yasgui.triply.cc/?endpoint=${endpoint}&query=${query}`;
 };
